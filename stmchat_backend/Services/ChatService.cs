ad9d66a672d593dedbe554a51b44daa1a9f2065c
Rynco Maekawa <lynzrand@outlook.com>
2020-06-23 11:26:20 +0800

    fuck typo +++

--- stmchat_backend/Services/ChatService.cs
+++ stmchat_backend/Services/ChatService.cs
@@ -338,7 +338,7 @@ namespace stmchat_backend
                 var group = await this._groups.AsQueryable().Where(group => group.name == g).SingleAsync();
                 if (!group.UserLatestRead.TryGetValue(userId, out var lastMessage))
                 {
-                    await this._groups.UpdateOneAsync(g => g.id == group.id, Builders<ChatGroup>.Update.Set($"UserLatestRead.{user}", ObjectId.Empty));
+                    await this._groups.UpdateOneAsync(g => g.id == group.id, Builders<ChatGroup>.Update.Set($"UserLatestRead.{user.Username}", ObjectId.Empty));
                     lastMessage = ObjectId.Empty;
                 }
                 var groupLogCollection = this.database.GetCollection<WsSendChatMsg>(g);
@@ -388,7 +388,7 @@ namespace stmchat_backend
                 var group = await this._groups.AsQueryable().Where(group => group.name == g).SingleAsync();
                 if (!group.UserLatestRead.TryGetValue(userId, out var lastUnread))
                 {
-                    await this._groups.UpdateOneAsync(g => g.id == group.id, Builders<ChatGroup>.Update.Set($"UserLatestRead.{user}", ObjectId.Empty));
+                    await this._groups.UpdateOneAsync(g => g.id == group.id, Builders<ChatGroup>.Update.Set($"UserLatestRead.{user.Username}", ObjectId.Empty));
                     lastUnread = ObjectId.Empty;
                 }
                 var groupLogCollection = this.database.GetCollection<WsSendChatMsg>(g);
